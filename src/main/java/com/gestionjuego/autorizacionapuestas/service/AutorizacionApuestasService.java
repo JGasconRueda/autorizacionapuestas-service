35f67c9e3e7de473255d8db8a3fee02afcb03329
Jesus Gascon <jgasconrueda@gmail.com>
2022-01-11 19:20:02 +0100

    sleuth and zipkin

--- src/main/java/com/gestionjuego/autorizacionapuestas/service/AutorizacionApuestasService.java
+++ src/main/java/com/gestionjuego/autorizacionapuestas/service/AutorizacionApuestasService.java
@@ -1,20 +1,20 @@
 package com.gestionjuego.autorizacionapuestas.service;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
+import com.gestionjuego.autorizacionapuestas.entity.AutorizacionApuestas;
+import com.gestionjuego.autorizacionapuestas.exceptions.AutorizacionApuestasException;
+import com.gestionjuego.autorizacionapuestas.repository.AutorizacionApuestasRepository;
+import com.gestionjuego.autorizacionapuestas.vo.ResponseTemplateVO;
+import com.gestionjuego.autorizacionapuestas.vo.Ticket;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import com.gestionjuego.autorizacionapuestas.entity.AutorizacionApuestas;
-import com.gestionjuego.autorizacionapuestas.exceptions.AutorizacionApuestasException;
-import com.gestionjuego.autorizacionapuestas.exceptions.TicketErrorException;
-import com.gestionjuego.autorizacionapuestas.repository.AutorizacionApuestasRepository;
-import com.gestionjuego.autorizacionapuestas.vo.ResponseTemplateVO;
-import com.gestionjuego.autorizacionapuestas.vo.Ticket;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AutorizacionApuestasService {
@@ -24,26 +24,32 @@ public class AutorizacionApuestasService {
 
 	@Autowired
 	private RestTemplate restTemplate;
-	
+
+	private Logger logger = LoggerFactory.getLogger(AutorizacionApuestasService.class);
+
 	public AutorizacionApuestas save(AutorizacionApuestas autorizacionapuestas) {
+		logger.info("AutorizacionApuestasService - save: "+autorizacionapuestas.toString());
 		return autorizacionApuestasRepository.save(autorizacionapuestas);
 	}
 
 	public List<ResponseTemplateVO> getall() {
 		List<ResponseTemplateVO> list = new ArrayList<>();
 		autorizacionApuestasRepository.findAll().forEach(a->list.add(new ResponseTemplateVO(a, getTickeyById(a.getTicketId()))));
+		logger.info("AutorizacionApuestasService - getall: Return "+list.size()+" autorizaciones");
         return list;
 	}
 
 	public void remove(Long id) {
+		logger.info("AutorizacionApuestasService - remove:Id "+id);
 		autorizacionApuestasRepository.deleteById(id);
-		
 	}
 
 	public AutorizacionApuestas update(AutorizacionApuestas autorizacionApuestas) {
+		logger.info("AutorizacionApuestasService - update: autorizacionApuestas "+autorizacionApuestas.toString());
 		Optional<AutorizacionApuestas> optional = autorizacionApuestasRepository.findById(autorizacionApuestas.getId());
 		
 		if(!optional.isPresent()) {
+			logger.error("AutorizacionApuestasService - update: AutorizacionApuestasNotFound");
 			throw new AutorizacionApuestasException("AutorizacionApuestas with id "+autorizacionApuestas.getId()+" not found");
 		}
 		
@@ -55,17 +61,20 @@ public class AutorizacionApuestasService {
 	}
 
 	public Optional<ResponseTemplateVO> getAutorizacionApuestasWithTicket(Long id) {
+		logger.info("AutorizacionApuestasService - getAutorizacionApuestasWithTicket:Id "+id);
 		ResponseTemplateVO vo = new ResponseTemplateVO();
 		Ticket ticket = new Ticket();
 		
 		Optional<AutorizacionApuestas> apuestas = autorizacionApuestasRepository.findById(id);
 		
 		if(!apuestas.isPresent()) {
+			logger.info("AutorizacionApuestasService - getAutorizacionApuestasWithTicket: Not found");
 			return Optional.empty();
 		}
 		try{
 			ticket = getTickeyById(apuestas.get().getTicketId());
 		}catch (Exception e) {
+			logger.error("AutorizacionApuestasService - getAutorizacionApuestasWithTicket: TicketNotFound");
 			return Optional.empty();
 		}
 		
@@ -76,6 +85,7 @@ public class AutorizacionApuestasService {
 	}
 
 	private Ticket getTickeyById(Long id) {
+		logger.info("AutorizacionApuestasService - getTickeyById:TicketId "+id);
 		Ticket ticket = restTemplate.getForObject("http://TICKET-SERVICE/tickets/"+id, Ticket.class);
 		return ticket;
 	}
